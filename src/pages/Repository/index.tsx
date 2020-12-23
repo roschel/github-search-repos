afbd940cfbd4c227e6960865afaf9e8f392edf87
roschel <joaomroschel@gmail.com>
2020-12-23 12:23:38 -0300

    style repos details

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,6 +1,61 @@
 import React from 'react';
+import { Link, useRouteMatch } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Header, RepositoryInfo, Issues } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img src="" alt="" />
+          <div>
+            <strong>Repos name</strong>
+            <p>Descrição</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>issues</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="dfsdfdf">
+          <div>
+            <strong>agsdfgd</strong>
+            <p>fghd</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
+
 export default Repository;
